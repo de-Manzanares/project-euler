a2812dca3a6a8c917f73e160193a9789257a8d4c
schauss <schauss@dmanz.org>
2025-05-02 12:40:02 -0700

    1

--- 1/1.asm
+++ 1/1.asm
@@ -1,46 +1,46 @@
-section .data
-
-sum dq 0
+; Project Euler - Multiples of 3 or 5
+; https://projecteuler.net/problem=1
+; NASM, x86-64 Linux
 
 section .text
 
 global _start
 _start:
 
-  mov ecx, 999
+xor   r9,   r9    ; result 
+mov   rcx,  999
 
 sumLoop:
-  jmp checkThree
-  jmp checkFive
-  afterAdd:
-    loop sumLoop
-
-jmp exit
+jmp   checkThree
+jmp   checkFive
+afterAdd:
+loop  sumLoop
+jmp   exit
 
 checkThree:
-  mov   r8d,  3
-  xor   edx,  edx 
-  mov   eax,  ecx 
-  div   r8d 
-  cmp   edx,  0
-  je    add
-  jmp   checkFive
+mov   r8d,  3
+xor   edx,  edx 
+mov   eax,  ecx 
+div   r8d 
+cmp   edx,  0
+je    add
+jmp   checkFive
   
 checkFive:
-  mov   r8d,  5
-  xor   edx,  edx 
-  mov   eax,  ecx 
-  div   r8d 
-  cmp   edx,  0
-  je    add
-  jmp   afterAdd
+mov   r8d,  5
+xor   edx,  edx 
+mov   eax,  ecx 
+div   r8d 
+cmp   edx,  0
+je    add
+jmp   afterAdd
 
 add:
-  add [sum], ecx
-  jmp afterAdd 
+add   r9,   rcx
+jmp   afterAdd 
 
 exit:
-  mov     rax, 60
-  mov     rdi, 0
-  syscall
+mov     rax, 60
+mov     rdi, 0
+syscall
 
